111a722208308cfb9b23f40a1122b46775234ce0
Lisa Carlson <lisa.carlson85@gmail.com>
2015-08-07 11:01:17 -0600

    House is initialized with num of doors and num of windows

--- spec/house_spec.js
+++ spec/house_spec.js
@@ -5,6 +5,14 @@ describe ('House', function() {
     it ('uses the numberOfDoors argument as a property', function() {
       var house = new House (100); 
       expect(house.numberOfDoors).toEqual(100);
-    })
-  })
-})
+    });
+
+    it('uses the numberOfWindows argument as a property', function () {
+        var house = new House(0,200);
+
+        expect(house.numberOfWindows).toEqual(200);
+      });
+    });
+
+
+});
