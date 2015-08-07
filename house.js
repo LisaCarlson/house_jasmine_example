d4a9c5cd8c550bb392a650226d9f6472ba9b30af
Lisa Carlson <lisa.carlson85@gmail.com>
2015-08-07 11:35:58 -0600

    complete testing suite passes

--- house.js
+++ house.js
@@ -1,6 +1,26 @@
 function House (numberOfDoors, numberOfWindows) {
   this.numberOfDoors = numberOfDoors;
   this.numberOfWindows = numberOfWindows;
+  this.toString = function() {
+    if (this.numberOfDoors > 1 && this.numberOfWindows === 1) {
+      return 'The house has ' + numberOfDoors + ' doors and ' + numberOfWindows + ' window';
+    }
+    else if ((this.numberOfDoors > 1 || this.numberOfDoors === 0) && (this.numberOfWindows > 1 || this.numberOfWindows === 0)) {
+      return 'The house has ' + numberOfDoors + ' doors and ' + numberOfWindows + ' windows';
+    }
+    else if (this.numberOfDoors === 1 && this.numberOfWindows > 1) {
+      return 'The house has ' + numberOfDoors + ' door and ' + numberOfWindows + ' windows';
+    }
+    else if (this.numberOfDoors === 1 && this.numberOfWindows === 1) {
+      return 'The house has ' + numberOfDoors + ' door and ' + numberOfWindows + ' window';
+    }
+  };
+
 };
+
+
+
+
+
 
 module.exports = House;
--- spec/house_spec.js
+++ spec/house_spec.js
@@ -13,6 +13,34 @@ describe ('House', function() {
         expect(house.numberOfWindows).toEqual(200);
       });
     });
+  
+  describe('methods', function () {
+      describe('#toString', function () {
 
+        it('returns a string with correct pluralization for multiple doors and 1 window', function () {
+          var house = new House(3,1);
+          expect(house.toString()).toEqual('The house has 3 doors and 1 window');
+        });
 
-});
+        it('returns a string with correct pluralization for multiple doors and multiple windows', function () {
+          var house = new House(3,5);
+          expect(house.toString()).toEqual('The house has 3 doors and 5 windows');
+        });
+
+        it('returns a string with correct pluralization for 1 door and multiple windows', function () {
+          var house = new House(1,65);
+          expect(house.toString()).toEqual('The house has 1 door and 65 windows');
+        });
+
+        it('returns a string with correct pluralization for 1 door and 1 window', function () {
+          var house = new House(1,1);
+          expect(house.toString()).toEqual('The house has 1 door and 1 window');
+        });
+
+        it('returns a string with correct pluralization for 0 doors and 0 windows', function () {
+          var house = new House(0,0);
+          expect(house.toString()).toEqual('The house has 0 doors and 0 windows');
+        });
+      });
+    });
+  });
